b58ecf5014bf1263fb0b6ca58ac65a741c056ccd
Eloy <this.eloydev@gmail.com>
2026-06-12 12:12:19 -0300

    adiciona metodos de checagem e resultado na classe Jokenpo

--- src/jokenpo/logica/Jokenpo.java
+++ src/jokenpo/logica/Jokenpo.java
@@ -16,11 +16,43 @@ public class Jokenpo{
         jogador1Jogada = null;
         jogador2Jogada = null;
     }
-    
-    // verifica se o jogador escolheu uma jogada valida
-    public void realizarJogada(String jogada){
+     
+    public void realizarJogada(int jogador, String jogada){
+        // verifica se o jogador é válido
+        if(jogador != 1 && jogador != 2){
+            throw new GameException("Jogador inválido! Escolha entre 1 ou 2.");
+        }
+        // verifica se o jogador escolheu uma jogada valida
         if(!jogadas.contains(jogada)){
-            throw new GameException("Movimento inválido!");
+            throw new GameException("Movimento inválido! Escolha entre Pedra, Papel ou Tesoura.");
+        }
+        // marca jogadas
+        if(jogador == 1){
+            jogador1Jogada = jogada;
+        }else{
+            jogador2Jogada = jogada;
+        }        
+    }
+    
+    // verifica se os dois jogaram
+    public boolean jogaram(){
+        return jogador1Jogada != null && jogador2Jogada != null;
+    }
+    
+    public String ganhador(){
+        if(!jogaram()){
+            return null;
+        }
+        if(jogador1Jogada.equals(jogador2Jogada)){
+            return "Empate!";
+        }
+        if((jogador1Jogada.equals("Pedra") && jogador2Jogada.equals("Tesoura")) ||
+           (jogador1Jogada.equals("Papel") && jogador2Jogada.equals("Pedra")) ||
+           (jogador1Jogada.equals("Tesoura") && jogador2Jogada.equals("Papel")) ){
+            return "Jogador 1 ganhou!";
+        }else{
+            return "Jogador 2 ganhou!";
         }
     }
+    
 }
